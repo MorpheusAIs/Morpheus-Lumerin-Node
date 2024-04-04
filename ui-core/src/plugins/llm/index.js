5f9b43ddbf09737184942e03266748cd821255c1
Scott Terry <srt0422@gmail.com>
2024-04-03 22:47:21 -0700

    update model plugin initiation, add some testing, retrograde some syntax; more retro grading needed

--- ui-core/src/plugins/llm/index.js
+++ ui-core/src/plugins/llm/index.js
@@ -1,52 +1,55 @@
-import olama from "./olama";
+const olama = require('./ollama')
 
-function createPlugin () {
-    /**
-     * Start the plugin.
-     *
-     * @param {object} params The start parameters.
-     * @param {object} params.config The configuration options.
-     * @param {object} params.eventBus The cross-plugin event emitter.
-     * @param {object} params.plugins All other plugins.
-     * @returns {{api:object,events:string[],name:string}} The plugin API.
-     */
-    function start ({ config, eventBus, plugins }) {
-      // debug.enabled = config.debug;
-  
-      const {  } = config;
-      const {  } = plugins;
-    
-      // eventBus.on('coin-block', emitLumerinStatus);
-  
-      // Collect meta parsers
-      const metaParsers = Object.assign({},
-        // {
-        //   // auction: auctionEvents.auctionMetaParser,
-        //   // converter: converterEvents.converterMetaParser,
-        //   export: porterEvents.exportMetaParser,
-        //   import: porterEvents.importMetaParser,
-        //   importRequest: porterEvents.importRequestMetaParser
-        // },
-      );
-  
-      // Build and return API
-      return {
-        api: {
-          ...olama
-        },
-        events: [
-        ],
-        name: 'llm'
-      };
-    }
-  
-    /**
-     * Stop the plugin.
-     */
-    function stop () {}
-  
+function createPlugin() {
+  /**
+   * Start the plugin.
+   *
+   * @param {object} params The start parameters.
+   * @param {object} params.config The configuration options.
+   * @param {object} params.eventBus The cross-plugin event emitter.
+   * @param {object} params.plugins All other plugins.
+   * @returns {{api:object,events:string[],name:string}} The plugin API.
+   */
+  function start({ config, eventBus, plugins }) {
+    // debug.enabled = config.debug;
+
+    const { defaultLLM } = config
+    const {} = plugins
+
+    olama.init({
+      model: config.defaultLLM,
+    })
+    // eventBus.on('coin-block', emitLumerinStatus);
+
+    // Collect meta parsers
+    const metaParsers = Object.assign(
+      {}
+      // {
+      //   // auction: auctionEvents.auctionMetaParser,
+      //   // converter: converterEvents.converterMetaParser,
+      //   export: porterEvents.exportMetaParser,
+      //   import: porterEvents.importMetaParser,
+      //   importRequest: porterEvents.importRequestMetaParser
+      // },
+    )
+
+    // Build and return API
     return {
-      start,
-      stop
-    };
+      api: {
+        ...olama,
+      },
+      events: [],
+      name: 'llm',
+    }
   }
+
+  /**
+   * Stop the plugin.
+   */
+  function stop() {}
+
+  return {
+    start,
+    stop,
+  }
+}
