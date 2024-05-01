5a47fbdc15185c34afee66e37a978fb1878c6f66
Oleksandr Shevchuk <shev@titan.io>
2024-05-01 13:15:25 +0200

    feat: integrate distribution recalculation from L1

--- smart-contracts/contracts/DiamondInit.sol
+++ smart-contracts/contracts/DiamondInit.sol
@@ -13,8 +13,11 @@ import { IDiamondLoupe } from "./diamond/interfaces/IDiamondLoupe.sol";
 import { IDiamondCut } from "./diamond/interfaces/IDiamondCut.sol";
 import { IERC173 } from "./diamond/interfaces/IERC173.sol";
 import { IERC165 } from "./diamond/interfaces/IERC165.sol";
-import { LibAppStorage, AppStorage, Session } from "./AppStorage.sol";
+import { LibAppStorage, AppStorage, Session, Pool } from "./AppStorage.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Diamond contract resources
+// https://github.com/mudgen/awesome-diamonds/blob/main/README.md
 
 // It is expected that this contract is customized if you want to deploy your diamond
 // with data from a deployment script. Use the init function to initialize state variables
@@ -40,6 +43,8 @@ contract DiamondInit {
 
     s.stakeDelay = 0;
     s.tokenAccount = _tokenAccount;
+
+    // we need to add a dummy session to avoid index 0
     s.sessions.push(
       Session({
         id: bytes32(0),
@@ -55,6 +60,16 @@ contract DiamondInit {
         closedAt: 0
       })
     );
+
+    // default values for pool
+    // watch also (SessionRouter.sol).setPoolConfig
+    s.pool = Pool({
+      payoutStart: 1707393600,
+      decreaseInterval: 86400,
+      initialReward: 3456000000000000000000,
+      rewardDecrease: 592558728240000000
+    });
+
     // EIP-2535 specifies that the `diamondCut` function takes two optional
     // arguments: address _init and bytes calldata _calldata
     // These arguments are used to execute an arbitrary function using delegatecall
