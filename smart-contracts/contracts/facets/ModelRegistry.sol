60003d57c22455820a249cd4b19ca0178ada92a7
Oleksandr Shevchuk <shev@titan.io>
2024-05-08 19:06:37 +0200

    fix: modelGetAll indexes

--- smart-contracts/contracts/facets/ModelRegistry.sol
+++ smart-contracts/contracts/facets/ModelRegistry.sol
@@ -33,12 +33,16 @@ contract ModelRegistry {
     return s.activeModels.count();
   }
 
-  function modelGetAll() public view returns (Model[] memory) {
-    Model[] memory _models = new Model[](s.activeModels.count());
-    for (uint i = 0; i < s.activeModels.count(); i++) {
-      _models[i] = s.modelMap[s.activeModels.keyAtIndex(i)];
+  function modelGetAll() public view returns (bytes32[] memory, Model[] memory) {
+    uint256 len = s.activeModels.count();
+    Model[] memory _models = new Model[](len);
+    bytes32[] memory ids = new bytes32[](len);
+    for (uint i = 0; i < len; i++) {
+      bytes32 id = s.activeModels.keyAtIndex(i);
+      ids[i] = id;
+      _models[i] = s.modelMap[id];
     }
-    return _models;
+    return (ids, _models);
   }
 
   function modelGetByIndex(uint index) public view returns (bytes32 modelId, Model memory model) {
