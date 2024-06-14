294e182583a15b021cfbebe3be6340f9482487df
Oleksandr Shevchuk <shev@titan.io>
2024-06-14 16:36:26 +0200

    fix: cleanup core

--- ui-core/src/plugins/eth/check-chain.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const checkChain = (web3, chainId) =>
-  web3.eth.getChainId()
-    .then(id => {
-      return id === +chainId || Promise.reject(new Error('Wrong chain'))
-    });
-
-module.exports = checkChain;
