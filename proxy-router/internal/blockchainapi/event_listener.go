c2af6c72be9a76c4a11b31905f8bc35ae8b95fad
Aleksandr Kukharenko <aleksandr@titan.io>
2024-09-30 11:25:36 +0300

    fix: improve log

--- proxy-router/internal/blockchainapi/event_listener.go
+++ proxy-router/internal/blockchainapi/event_listener.go
@@ -100,7 +100,13 @@ func (e *EventsListener) handleSessionOpened(event *sessionrouter.SessionRouterS
 	}
 
 	modelID := lib.BytesToString(session.ModelAgentId[:])
-	modelConfig := e.modelConfigLoader.ModelConfigFromID(modelID)
+
+	var modelConfig *config.ModelConfig
+	if session.Provider.Hex() == address.Hex() {
+		modelConfig = e.modelConfigLoader.ModelConfigFromID(modelID)
+	} else {
+		modelConfig = &config.ModelConfig{}
+	}
 
 	err = e.store.AddSession(&storages.Session{
 		Id:           sessionId,
