73bc936ed6d4c5053a12008d12088683d7e9257a
shev <shev@titan.io>
2024-10-09 21:29:13 +0200

    feat: ethprovider with multiple endpoints

---
+++ proxy-router/internal/repositories/ethclient/rpcclient.go
@@ -0,0 +1,14 @@
+package ethclient
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+type RPCClient interface {
+	Close()
+	BatchCallContext(context.Context, []rpc.BatchElem) error
+	CallContext(ctx context.Context, result interface{}, method string, args ...interface{}) error
+	EthSubscribe(ctx context.Context, channel interface{}, args ...interface{}) (*rpc.ClientSubscription, error)
+}
