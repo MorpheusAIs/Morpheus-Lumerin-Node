5dbb7d0bf853a813988f3b3d03971461077bb9f6
Oleksandr <oleksandr.fedorenko@distributedlab.com>
2025-01-06 16:25:40 +0200

    add the deployment script

--- smart-contracts/deploy/3_delegate_protocol.migration copy.ts
+++ smart-contracts/deploy/3_delegate_protocol.migration copy.ts
@@ -3,30 +3,29 @@ import { Deployer } from '@solarity/hardhat-migrate';
 import { parseConfig } from './helpers/config-parser';
 
 import {
-  DelegatorFactory__factory,
+  DelegateFactory__factory,
   ERC1967Proxy__factory,
-  ProvidersDelegator__factory,
+  ProvidersDelegate__factory,
 } from '@/generated-types/ethers';
 import { wei } from '@/scripts/utils/utils';
 
 module.exports = async function (deployer: Deployer) {
   const config = parseConfig();
 
-  const providersDelegatorImpl = await deployer.deploy(ProvidersDelegator__factory);
-  const delegatorFactoryImpl = await deployer.deploy(DelegatorFactory__factory);
+  const providersDelegatorImpl = await deployer.deploy(ProvidersDelegate__factory);
+  const delegatorFactoryImpl = await deployer.deploy(DelegateFactory__factory);
   const proxy = await deployer.deploy(ERC1967Proxy__factory, [await delegatorFactoryImpl.getAddress(), '0x']);
 
-  const delegatorFactory = await deployer.deployed(DelegatorFactory__factory, await proxy.getAddress());
+  const delegatorFactory = await deployer.deployed(DelegateFactory__factory, await proxy.getAddress());
 
-  await delegatorFactory.DelegatorFactory_init(config.lumerinProtocol, providersDelegatorImpl);
+  await delegatorFactory.DelegateFactory_init(config.lumerinProtocol, providersDelegatorImpl, 60 * 60);
 
   await delegatorFactory.deployProxy(
     '0x19ec1E4b714990620edf41fE28e9a1552953a7F4',
     wei(0.2, 25),
     'First Subnet',
     'Custom endpoint',
-    60 * 60 * 1,
-    60 * 30,
+    Math.floor((Date.now() / 1000)) + 24 * 60 * 60,
   );
 };
 
