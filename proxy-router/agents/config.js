8ff17988793f3d15bc9b982046288abc03de0d68
Aleksandr Kukharenko <aleksandr@titan.io>
2025-01-30 17:52:32 +0200

    feat: agent example

---
+++ proxy-router/agents/config.js
@@ -0,0 +1,15 @@
+const proxyRouterUrl = 'http://localhost:8084';
+const agentUsername = 'agent-test-1';
+const agentPassword = '123456';
+const agentPerms = ['get_local_models', 'get_balance', 'chat'];
+
+const modelId =
+  "0xe086adc275c99e32bb10b0aff5e8bfc391aad18cbb184727a75b2569149425c1";
+
+module.exports = {
+  proxyRouterUrl,
+  agentUsername,
+  agentPassword,
+  agentPerms,
+  modelId,
+};
