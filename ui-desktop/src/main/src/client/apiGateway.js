52d54ce9f3983b8f73ed5357cd8d64683773e48b
Aleksandr Kukharenko <aleksandr@titan.io>
2025-02-07 11:43:10 +0200

    feat: get cookie path from proxy

--- ui-desktop/src/main/src/client/apiGateway.js
+++ ui-desktop/src/main/src/client/apiGateway.js
@@ -1,37 +1,41 @@
 import config from '../../config'
 import fs from 'fs'
 import os from 'os'
-import path from 'path'
-import { app } from 'electron'
 
-let cookieFilePath = config.chain.proxyRouterCookieFilePath;
-if (app.isPackaged) {
-    let basePath;
+let auth = null;
 
-    if (process.platform === 'darwin' && process.execPath.includes('.app')) {
-      const appBundlePath = process.execPath.match(/(.*\.app)/)[0];
-      basePath = path.dirname(appBundlePath);
-    } else {
-      basePath = path.dirname(process.execPath);
+const getAuthHeaders = async () => {
+    if (auth) {
+        return auth;
     }
-    
-    cookieFilePath = path.join(basePath, config.chain.proxyRouterCookieFilePath);
-}
 
-const isWindows = os.platform() === 'win32' || os.platform() === 'win64';
-cookieFilePath = isWindows ? cookieFilePath.replace(/\//g, '\\') : cookieFilePath;
+    try {
+        const path = `${config.chain.localProxyRouterUrl}/auth/cookie/path`
+        const response = await fetch(path);
+        const body = await response.json();
+        let cookieFilePath = body.path;
 
-const cookieFile = fs.readFileSync(cookieFilePath, 'utf8').trim();
-const [username, password] = cookieFile.split(':');
-const auth = {
-    Authorization: `Basic ${Buffer.from(`${username}:${password}`, 'utf-8').toString('base64')}`
+        const isWindows = os.platform() === 'win32' || os.platform() === 'win64';
+        cookieFilePath = isWindows ? cookieFilePath.replace(/\//g, '\\') : cookieFilePath;
+
+        const cookieFile = fs.readFileSync(cookieFilePath, 'utf8').trim();
+        const [username, password] = cookieFile.split(':');
+        auth = {
+            Authorization: `Basic ${Buffer.from(`${username}:${password}`, 'utf-8').toString('base64')}`
+        }
+        return auth;
+    }
+    catch (e) {
+        console.log("Error", e)
+        throw e;
+    }
 }
 
 const getAllModels = async () => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/blockchain/models`
         const response = await fetch(path, {
-            headers: auth,
+            headers: await getAuthHeaders(),
             method: "GET"
         });
         const data = await response.json();
@@ -47,7 +51,7 @@ const getBalances = async () => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/blockchain/balance`
         const response = await fetch(path, {
-            headers: auth
+            headers: await getAuthHeaders(),
         });
         const data = await response.json();
         return data;
@@ -66,7 +70,7 @@ const sendEth = async (to, amount) => {
             body: JSON.stringify({
                 to, amount
             }),
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const data = await response.json();
         return data.txHash;
@@ -85,7 +89,7 @@ const sendMor = async (to, amount) => {
             body: JSON.stringify({
                 to, amount
             }),
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const data = await response.json();
         return data.txHash;
@@ -100,7 +104,7 @@ const getTransactions = async (payload) => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/blockchain/transactions?page=${payload.page}&limit=${payload.pageSize}`
         const response = await fetch(path, {
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const data = await response.json();
         return data.transactions;
@@ -128,7 +132,7 @@ const getTodaysBudget = async () => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/blockchain/sessions/budget`;
         const response = await fetch(path, {
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const body = await response.json();
         return body.budget;
@@ -143,7 +147,7 @@ const getTokenSupply = async () => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/blockchain/token/supply`;
         const response = await fetch(path, {
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const body = await response.json();
         return body.supply;
@@ -165,7 +169,7 @@ const getChatHistoryTitles = async () => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/v1/chats`;
         const response = await fetch(path, {
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const body = await response.json();
         return body;
@@ -190,7 +194,7 @@ const getChatHistoryTitles = async () => {
  * @property {string} prompt
  * @property {number} promptAt
  * @property {number} responseAt
- */ 
+ */
 
 /**
  * @typedef ChatPrompt
@@ -206,18 +210,18 @@ const getChatHistoryTitles = async () => {
  * @returns {Promise<ChatHistory>}
 */
 const getChatHistory = async (chatId) => {
-   try {
-       const path = `${config.chain.localProxyRouterUrl}/v1/chats/${chatId}`;
-       const response = await fetch(path, {
-              headers: auth
-       });
-       const body = await response.json();
-       return body;
-   }
-   catch (e) {
-       console.log("Error", e)
-       return null;
-   }
+    try {
+        const path = `${config.chain.localProxyRouterUrl}/v1/chats/${chatId}`;
+        const response = await fetch(path, {
+            headers: await getAuthHeaders()
+        });
+        const body = await response.json();
+        return body;
+    }
+    catch (e) {
+        console.log("Error", e)
+        return null;
+    }
 }
 
 /**
@@ -229,7 +233,7 @@ const deleteChatHistory = async (chatId) => {
         const path = `${config.chain.localProxyRouterUrl}/v1/chats/${chatId}`;
         const response = await fetch(path, {
             method: "DELETE",
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const body = await response.json();
         return body.result;
@@ -238,20 +242,20 @@ const deleteChatHistory = async (chatId) => {
         console.log("Error", e)
         return false;
     }
- }
+}
 
- /**
- * @param {string} chatId
- * @param {string} title
- * @returns {Promise<boolean>}
+/**
+* @param {string} chatId
+* @param {string} title
+* @returns {Promise<boolean>}
 */
-const updateChatHistoryTitle = async ({ id, title}) => {
+const updateChatHistoryTitle = async ({ id, title }) => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/v1/chats/${id}`;
         const response = await fetch(path, {
             method: "POST",
             body: JSON.stringify({ title }),
-            headers: auth
+            headers: await getAuthHeaders()
         });
         const body = await response.json();
         return body.result;
@@ -260,26 +264,26 @@ const updateChatHistoryTitle = async ({ id, title}) => {
         console.log("Error", e)
         return false;
     }
- }
+}
 
-  /**
- * @param {string} address
- * @param {string} endpoint
- * @returns {Promise<boolean>}
+/**
+* @param {string} address
+* @param {string} endpoint
+* @returns {Promise<boolean>}
 */
-const checkProviderConnectivity = async ({ address, endpoint}) => {
+const checkProviderConnectivity = async ({ address, endpoint }) => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/proxy/provider/ping`;
         const response = await fetch(path, {
             method: "POST",
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 providerAddr: address,
                 providerUrl: endpoint
-             }),
-            headers: auth
+            }),
+            headers: await getAuthHeaders()
         });
 
-        if(!response.ok) {
+        if (!response.ok) {
             return false;
         }
 
@@ -290,14 +294,12 @@ const checkProviderConnectivity = async ({ address, endpoint}) => {
         console.log("checkProviderConnectivity: Error", e)
         return false;
     }
- }
-
-const getAuthHeaders = async () => auth;
+}
 
 const clearEthNodeEnv = async () => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/config/ethNode`
-        const response = await fetch(path, { method: "DELETE", headers: auth });
+        const response = await fetch(path, { method: "DELETE", headers: await getAuthHeaders() });
         const data = await response.json();
         return data.status;
     }
@@ -310,7 +312,7 @@ const clearEthNodeEnv = async () => {
 const clearWallet = async () => {
     try {
         const path = `${config.chain.localProxyRouterUrl}/wallet`
-        const response = await fetch(path, { method: "DELETE", headers: auth });
+        const response = await fetch(path, { method: "DELETE", headers: await getAuthHeaders() });
         const data = await response.json();
         return data.status;
     }
