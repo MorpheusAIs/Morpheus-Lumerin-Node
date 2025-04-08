5021013227408a9619a098d381e26d32e20bde15
shev <shev@titan.io>
2025-04-08 15:36:49 +0200

    feat: build pkg

---
+++ ui-desktop/electron-builder.config.ts
@@ -0,0 +1,70 @@
+import { Configuration } from 'electron-builder'
+
+const config: Configuration = {
+  appId: 'com.electron.morpheus-ui',
+  productName: 'MorpheusUI',
+  directories: {
+    buildResources: 'buildResources'
+  },
+  files: [
+    '!**/.vscode/*',
+    '!src/*',
+    '!electron.vite.config.{js,ts,mjs,cjs}',
+    '!{.eslintignore,.eslintrc.cjs,.prettierignore,.prettierrc.yaml,dev-app-update.yml,CHANGELOG.md,README.md}',
+    '!{.env,.env.*,.npmrc,pnpm-lock.yaml}',
+    '!{tsconfig.json,tsconfig.node.json,tsconfig.web.json}',
+    '!services/*'
+  ],
+  asarUnpack: ['resources/**', 'pkg-scripts/**'],
+  win: {
+    executableName: 'morpheus-ui',
+    target: ['portable']
+  },
+  portable: {
+    artifactName: '${name}-${version}-${arch}-${os}.${ext}'
+  },
+  mac: {
+    executableName: 'MorpheusUI',
+    entitlements: 'buildResources/entitlements.mac.plist',
+    extendInfo: {
+      NSCameraUsageDescription: "Application requests access to the device's camera.",
+      NSMicrophoneUsageDescription: "Application requests access to the device's microphone.",
+      NSDocumentsFolderUsageDescription:
+        "Application requests access to the user's Documents folder.",
+      NSDownloadsFolderUsageDescription:
+        "Application requests access to the user's Downloads folder."
+    },
+    target: [
+      // 'dmg',
+      'pkg'
+    ],
+    notarize: false,
+    artifactName: '${name}-${version}-${arch}-${os}.${ext}'
+  },
+  pkg: {
+    artifactName: '${name}-${version}-${arch}-${os}.${ext}',
+    installLocation: '/Applications',
+    isRelocatable: false
+    // allowAnywhere: false,
+    // allowCurrentUserHome: false,
+    // allowRootDirectory: false,
+    // scripts: './pkg-scripts'
+  },
+  linux: {
+    target: ['AppImage'],
+    maintainer: 'mor.org',
+    category: 'Utility',
+    executableName: 'MorpheusUI',
+    artifactName: '${name}-${version}-${arch}-${os}.${ext}'
+  },
+  npmRebuild: false,
+  publish: {
+    provider: 'generic',
+    url: 'https://example.com/auto-updates'
+  },
+  electronDownload: {
+    mirror: 'https://npmmirror.com/mirrors/electron/'
+  }
+}
+
+export default config
