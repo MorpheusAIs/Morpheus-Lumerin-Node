f9ea516efc1d8d6e22182814b80b8be46bf0828a
Aleksandr Kukharenko <aleksandr@titan.io>
2025-10-20 18:19:06 +0300

    add public urls for base chain ids

--- proxy-router/internal/repositories/ethclient/public.go
+++ proxy-router/internal/repositories/ethclient/public.go
@@ -37,6 +37,23 @@ var publicRPCURLs = map[int][]string{
 		"https://arb-mainnet-public.unifra.io",
 		"https://arb-mainnet.g.alchemy.com/v2/demo",
 	},
+	84532: {
+		"https://base-sepolia.drpc.org",
+		"https://base-sepolia.gateway.tenderly.co",
+		"https://sepolia.base.org",
+		"https://base-sepolia-rpc.publicnode.com",
+		"https://base-sepolia.therpc.io",
+		"https://base-sepolia.blockpi.network/v1/rpc/public"
+	},
+	8453: {
+		"https://base.llamarpc.com",
+		"https://base-mainnet.public.blastapi.io",
+		"https://base.public.blockpi.network/v1/rpc/public",
+		"https://base.lava.build", 
+		"https://1rpc.io/base",
+		"https://base-rpc.publicnode.com",
+		"https://mainnet.base.org"
+	}
 }
 
 var ErrNotSupportedChain = fmt.Errorf("chain is not supported")
